40c9350c43b724cc4a5e04e6a73bb81c692707ff
Fabrielg <gfrancois2003@gmail.com>
2026-01-27 18:18:16 +0100

    feat: close window

--- src/main.c
+++ src/main.c
@@ -16,6 +16,28 @@ int	cub_init(t_cub *cub)
 	return (0);
 }
 
+static void	mlx_clear(t_libx *libx)
+{
+	if (!libx->mlx)
+		return ;
+	if (libx->window)
+		mlx_destroy_window(libx->mlx, libx->window);
+	mlx_destroy_display(libx->mlx);
+	if (libx->mlx)
+		free(libx->mlx);
+}
+
+int	close_window(t_cub *cub)
+{
+	if (!cub)
+		return (exit(0), 0);
+	if (cub->libx.img_data.img)
+		mlx_destroy_image(cub->libx.mlx, cub->libx.img_data.img);
+	mlx_clear(&cub->libx);
+	exit(0);
+	return (1);
+}
+
 int	main(void)
 {
 	t_cub	cub;
