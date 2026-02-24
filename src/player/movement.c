120ea3e68390cbe30b5b5a553fa53e82f1ed7afc
Fabrielg <gfrancois2003@gmail.com>
2026-02-24 20:11:46 +0100

    fix-rebase: add rotate_player current function

--- src/player/movement.c
+++ src/player/movement.c
@@ -40,62 +40,11 @@ int	move_player(t_cub *cub, float dir_x, float dir_y)
  */
 int	rotate_player(t_cub *cub, float delta)
 {
-	float	new_x;
-	float	new_y;
-
-	new_x = cub->player.position[0] - cosf(cub->player.angle_view) * MOVE_SPEED * cub->fps.delta_time;
-	new_y = cub->player.position[1] - sinf(cub->player.angle_view) * MOVE_SPEED * cub->fps.delta_time;
-
-	if (check_collision(&cub->map, new_x, new_y) == 0)
-	{
-		cub->player.position[0] = new_x;
-		cub->player.position[1] = new_y;
-	}
-	return (0);
-}
-
-int	move_left(t_cub *cub)
-{
-	float	new_x;
-	float	new_y;
-
-	new_x = cub->player.position[0] + cosf(cub->player.angle_view - M_PI_2) * MOVE_SPEED * cub->fps.delta_time;
-	new_y = cub->player.position[1] + sinf(cub->player.angle_view - M_PI_2) * MOVE_SPEED * cub->fps.delta_time;
-
-	if (check_collision(&cub->map, new_x, new_y) == 0)
-	{
-		cub->player.position[0] = new_x;
-		cub->player.position[1] = new_y;
-	}
-	return (0);
-}
-
-int	move_right(t_cub *cub)
-{
-	float	new_x;
-	float	new_y;
-
-	new_x = cub->player.position[0] + cosf(cub->player.angle_view + M_PI_2) * MOVE_SPEED * cub->fps.delta_time;
-	new_y = cub->player.position[1] + sinf(cub->player.angle_view + M_PI_2) * MOVE_SPEED * cub->fps.delta_time;
-
-	if (check_collision(&cub->map, new_x, new_y) == 0)
-	{
-		cub->player.position[0] = new_x;
-		cub->player.position[1] = new_y;
-	}
-	return (0);
-}
-
-int	rotate_left(t_cub *cub)
-{
-	cub->player.angle_view -= ROT_SPEED * cub->fps.delta_time;
-	return (0);
-}
-
-int	rotate_right(t_cub *cub)
-{
-	cub->player.angle_view += ROT_SPEED * cub->fps.delta_time;
-	return (0);
+	cub->player.angle_view += delta * ROT_SPEED * cub->fps.delta_time;
+	cub->player.angle_view = fmodf(cub->player.angle_view, 2 * M_PI);
+	if (cub->player.angle_view < 0)
+		cub->player.angle_view += 2 * M_PI;
+	return 0;
 }
 
 int	respawn(t_cub *cub)
